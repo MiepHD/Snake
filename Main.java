81b8da34ea732924f2f049a3a44a7b5354c38dfe
MiepHD <miep_hd@froxot.com>
2023-05-21 13:03:24 +0200

    Made it working again

--- Main.java
+++ Main.java
@@ -1,27 +1,34 @@
 import java.util.concurrent.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 /**
  * Beschreiben Sie hier die Klasse Main.
  * 
- * @author (Ihr Name) 
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Main {
-    public static void main(String[] args){
-        GUI gui = new GUI();
+    public static void main(String[] args) {
+        GUI gui = new GUI(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+
+            }
+        });
         Snake snake = new Snake();
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         Runnable runnable = new Runnable() {
             public void run() {
+                System.out.println("Tick");
                 ArrayList<Point> points = new ArrayList<Point>();
                 for (Coordinates coor : snake.getPositions()) {
                     points.add(new Point(coor, Color.BLUE));
                 }
-                gui.repaint();
+                gui.repaint(points);
             }
         };
-        executor.scheduleAtFixedRate(runnable, 0, 10, TimeUnit.MILLISECONDS);
-    } 
+        executor.scheduleAtFixedRate(runnable, 0, 1000, TimeUnit.MILLISECONDS);
+    }
 }
--- Snake.java
+++ Snake.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 /**
  * Beschreiben Sie hier die Klasse Snake.
  * 
- * @author (Ihr Name) 
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Snake {
     private ArrayList<Coordinates> positions;
-    private GUI gui;
+
     public Snake() {
-        this.positions.add(new Coordinates(7, 0));
+        (this.positions = new ArrayList<Coordinates>()).add(new Coordinates(7, 0));
     }
-    
+
     public ArrayList<Coordinates> getPositions() {
         return this.positions;
     }
